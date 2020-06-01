7a255bb0276609a69b3fb0db78dd1ee09d9210fa
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-01 14:38:15 +0300

    watch later init

--- routes/user_library/watchLater.js
+++ routes/user_library/watchLater.js
@@ -1,124 +1,78 @@
 const { Router } = require('express')
 const mongoose = require('mongoose')
+
 const UserPlaylist = require('../../models/UserPlaylist')
 const Film = require('../../models/Film')
+
 const router = Router()
 
-// router.put('/playlists/create', async(req, res) => {
-//     const { name, filmId } = req.body
+router.put('/watch_later/add', async(req, res) => {
+    const { userId, filmId } = req.body
 
-//     const playlist = {
-//         playlistName: name,
-//         films: [] 
-//     }
+    try {
+        const watchLaterList = await UserPlaylist.aggregate([
+            {
+                $match: {
+                    userId: mongoose.Types.ObjectId(userId),
+                    watchLater: mongoose.Types.ObjectId(filmId)
+                }
+            }
+        ])
+
+        if(watchLaterList.length) {
+            return res.status(404).json({
+                message: `Film in list already`
+            })
+        }
+
+        await UserPlaylist.updateOne(
+            { 
+                userId: mongoose.Types.ObjectId(userId)
+            }, 
+            { $push: {
+                'watchLater': mongoose.Types.ObjectId(filmId)
+            }
+        })
+
+        res.status(200).json({
+            message: `Film has been added`
+        })
+
+    } catch (err) {
+        res.status(400).json({
+            message: 'Something went wrong'
+        })
+    }
+})
+
+router.get('/later/:id', async(req, res) => {
     
-//     try {
-//         const playlistsList = await UserPlaylist.aggregate([
-//             {
-//                 $match: {
-//                     userId: mongoose.Types.ObjectId(userId),
-//                     // playlists: mongoose.Types.ObjectId(filmId)
-//                 }
-//             }
-//         ])
-
-//         if(playlistsList.length) {
-//             return res.status(404).json({
-//                 message: `${filmId} already added`
-//             })
-//         }
-
-//         await UserPlaylist.updateOne(
-//             { 
-//                 userId: mongoose.Types.ObjectId(userId)
-//             }, 
-//             { $push: {'playlists': playlist }
-//         })
-
-//         res.status(200).json({
-//             message: `Playlist ${name} has been created`
-//         })
-
-//     } catch (err) {
-//         res.status(400).json({
-//             message: 'Something went wrong'
-//         })
-//     }
-// })
-
-// router.put('/liked/remove', async(req, res) => {
-//     const { userId, filmId } = req.body
-    
-//     try {
-//         await UserPlaylist.updateOne(
-//             { 
-//                 userId: mongoose.Types.ObjectId(userId)
-//             }, 
-//             { $pull: {'liked': mongoose.Types.ObjectId(filmId) }
-//         })
-
-//         res.status(200).json({
-//             message: `${filmId} removed`
-//         })
-
-//     } catch (err) {
-//         res.status(400).json({
-//             message: 'Something went wrong'
-//         })
-//     }
-// })
-
-// router.get('/liked/:id', async(req, res) => {
-    
-//     try {
-//         const likedList = await UserPlaylist.findOne({ userId: mongoose.Types.ObjectId(req.params.id)})
-//         const likedMaped = likedList.liked.map(film => film)
-
-//         const likedListFilms = await Film.find({
-//             _id: {$in: likedMaped},
-//         })
-          
-//         const likedListReady = likedListFilms.map(opt => ({
-//             name: opt.name,
-//             _id: opt._id,
-//             img: opt.img
-//         }))
+    try {
+        const likedList = await UserPlaylist.findOne({ 
+            userId: mongoose.Types.ObjectId(req.params.id)}, 
+            {
+                watchLater: 1
+            })
         
-//         res.json(likedListReady)
+        const films = await Film.find({
+            _id: {
+                $in: likedList.watchLater
+            }
+        }, 
+        {
+            name: 1,
+            _id: 1,
+            img: 1
+        })
 
-//     } catch (err) {
-//         res.status(400).json({
-//             message: 'Something went wrong'
-//         })
-//     }
-// })
+     
+        res.status(200).json(films)
 
-// router.post('/like_status', async(req, res) => {
-
-//     const { filmId, userId } = req.body
-
-//     try {
-//         const likedList = await UserPlaylist.aggregate([
-//             {
-//                 $match: {
-//                     userId: mongoose.Types.ObjectId(userId),
-//                     liked: mongoose.Types.ObjectId(filmId)
-//                 }
-//             }
-//         ])
-
-//         if(likedList.length) {
-//             res.json(true)
-//         } else {
-//             res.json(false)
-//         }
-
-//     } catch (err) {
-//         res.status(400).json({
-//             message: 'Something went wrong'
-//         })
-//     }
-// })
-
+    } catch (err) {
+        res.status(400).json({
+            message: 'Something went wrong'
+        })
+    }
+})
 
 module.exports = router
