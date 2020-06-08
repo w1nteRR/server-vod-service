33630ab5a2848bba7188573747a486dd724c0fce
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-08 21:58:43 +0300

    routes updated

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express'
 import { authRouter } from './routes/auth'
+import { omdbRouter } from './routes/omdb'
+
+import { filmRouter } from './routes/film/film'
+import { filmAdminRouter } from './routes/film/film.admin'
+
 
 export const routes = () => {
 	const app = Router()
-	authRouter(app)
+	authRouter(app),
+	omdbRouter(app),
+	filmRouter(app),
+	filmAdminRouter(app)
 
 	return app
 }
