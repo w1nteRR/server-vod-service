49cea97dfb9abf5a4a7814a4ca462630cde884a6
Vatamanyuk Mykola <mykola796@gmail.com>
2020-06-09 14:17:15 +0300

    actor interface updated

--- src/interfaces/IActor.ts
+++ src/interfaces/IActor.ts
@@ -1,4 +1,18 @@
+import { Types } from "mongoose";
+
 export interface IActor {
     actorName: string,
-    films: Array<object>
+    films: Array<IActorFilms>
+}
+
+interface IActorFilms {
+    actorRole: string,
+    img: string,
+    _id: Types.ObjectId
+}
+
+export interface IActorCreate {
+    actorName: string,
+    actorRole: string,
+    filmId: string
 }
